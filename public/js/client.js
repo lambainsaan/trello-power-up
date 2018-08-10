df16ab70c53ed181b0c983492d756e29072d08ee
Rhitik Bhatt <bhattrhitik95@gmail.com>
2018-08-10 21:55:52 +0530

    Added badge to the cards with static data.

--- public/js/client.js
+++ public/js/client.js
@@ -1,21 +1,20 @@
-/* global TrelloPowerUp */
-
-var Promise = TrelloPowerUp.Promise;
-
-var BLACK_ROCKET_ICON = 'https://cdn.glitch.com/1b42d7fe-bda8-4af8-a6c8-eff0cea9e08a%2Frocket-ship.png?1494946700421';
-
 TrelloPowerUp.initialize({
-  // Start adding handlers for your capabilities here!
-  'card-buttons': function(t, options) {
+  'card-buttons': function (t, options) {
     return [{
-      icon: BLACK_ROCKET_ICON,
+      icon: 'https://cdn.glitch.com/c69415fd-f70e-4e03-b43b-98b8960cd616%2Frocket-ship-grey.png?1496162964717',
       text: 'Estimate Size',
-      callback: function(t) {
+      callback: function (t) {
         return t.popup({
           title: "Estimation",
           url: 'estimate.html',
         });
       }
+    }];
+  },
+  'card-badges': function (t, options) {
+    return [{
+      icon: 'https://cdn.glitch.com/c69415fd-f70e-4e03-b43b-98b8960cd616%2Frocket-ship-grey.png?1496162964717',
+      text: '3'
     }];
   },
 });
