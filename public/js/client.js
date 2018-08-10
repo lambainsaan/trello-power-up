93cd59d11accaf91ef233d5ffb71eb8575879e4a
Rhitik Bhatt <bhattrhitik95@gmail.com>
2018-08-10 11:27:31 +0530

    Added callback in the card-buttons.

--- public/js/client.js
+++ public/js/client.js
@@ -10,12 +10,12 @@ TrelloPowerUp.initialize({
 		return [{
     		icon: BLACK_ROCKET_ICON,
 			text: 'Estimate Size',
-	// callback: function(t) {
-	// return t.popup({
-	// title: "Estimation",
-	// url: 'estimate.html',
-	// });
-	// }
+	callback: function(t) {
+	return t.popup({
+	title: "Estimation",
+	url: './estimate.html',
+	});
+	}
 		}];
 	},
 });
